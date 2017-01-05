2ccfe0853a86ba62bb6adbbb6adbfd3e010d691e
Bob Bolender <bobthethree@gmail.com>
2017-01-05 10:18:58 -0600

    Add vuex, babel-preset-stage-3 for ... operator

--- src/Player.vue
+++ src/Player.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="player">
     {{ name }}
+    <button @click="decrement">-</button>
+    {{ sharedState.count }}
+    <button @click="increment">+</button>
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'player',
-  props: ['name']
+  props: ['name'],
+  computed: {
+    sharedState () {
+      return this.$store.state
+    }
+  },
+  methods: {
+    ...mapMutations([
+      'increment',
+      'decrement'
+    ])
+  }
 }
 </script>
 
