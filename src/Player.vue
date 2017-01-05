67fec7c02086533d793d5861caf7f7f97a6eba85
Bob Bolender <bobthethree@gmail.com>
2017-01-05 09:31:11 -0600

    Add PlayerBoard and Player components

---
+++ src/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="player">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'player',
+  props: ['name']
+}
+</script>
+
+<style scoped>
+.player {
+  border: 1px solid #000;
+  min-width: 200px;
+  min-height: 200px;
+  padding: 5px;
+  text-align: left;
+}
+</style>
+
---
+++ src/PlayerBoard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="PlayerBoard">
+    <input name="playerlist" v-model="playerlist">
+    <ul>
+      <li v-for="player in players">
+        <Player :name="player"></Player>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Player from './Player.vue';
+
+export default {
+  name: 'PlayerBoard',
+  data () {
+    return {
+      msg: 'PlayerBoard',
+      playerlist: 'Bob,Elvira,Ted,Charleen,Carol'
+    }
+  },
+  computed: {
+    players: function(){
+      return this.playerlist.split(',');
+    }
+  },
+  components: {
+    Player
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#PlayerBoard {
+  background-color: #eee;
+  input { width: 100%; }
+  ul {
+    max-width: 630px;;
+    margin: auto;
+    padding: 100px 0;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  li {
+    display: flex;
+    margin: 5px;
+  }
+}
+</style>
+
